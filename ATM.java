21d55ade156916b0e84b480206d432fba660373a
Sonu Daryani <100096513+sonu7524@users.noreply.github.com>
2022-11-21 22:31:31 +0530

    ATM Transaction

---
+++ ATM.java
@@ -0,0 +1,236 @@
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.lang.*;
+public class ATM {
+
+	public static void main(String[] args) throws IOException {
+		OptionMenu optionMenu = new OptionMenu();
+		introduction();
+		optionMenu.mainMenu();
+	}
+
+	public static void introduction() {
+		System.out.println("Welcome to the ATM Project!");
+	}
+}
+class Account {
+	// variables
+	private int customerNumber;
+	private int pinNumber;
+	private double checkingBalance = 0;
+
+	Scanner input = new Scanner(System.in);
+
+	public Account() {
+	}
+
+	public Account(int customerNumber, int pinNumber) {
+		this.customerNumber = customerNumber;
+		this.pinNumber = pinNumber;
+	}
+
+	public Account(int customerNumber, int pinNumber, double checkingBalance) {
+		this.customerNumber = customerNumber;
+		this.pinNumber = pinNumber;
+		this.checkingBalance = checkingBalance;
+	}
+
+	public int setCustomerNumber(int customerNumber) {
+		this.customerNumber = customerNumber;
+		return customerNumber;
+	}
+
+	public int getCustomerNumber() {
+		return customerNumber;
+	}
+
+	public int setPinNumber(int pinNumber) {
+		this.pinNumber = pinNumber;
+		return pinNumber;
+	}
+
+	public int getPinNumber() {
+		return pinNumber;
+	}
+
+	public double getCheckingBalance() {
+		return checkingBalance;
+	}
+
+	public double calcCheckingWithdraw(double amount) {
+		checkingBalance = (checkingBalance - amount);
+		return checkingBalance;
+	}
+
+	public double calcCheckingDeposit(double amount) {
+		checkingBalance = (checkingBalance + amount);
+		return checkingBalance;
+	}
+
+	void credit(){ 
+        System.out.println("Enter the Amount: ");
+		int amt = input.nextInt();
+		calcCheckingDeposit(amt);
+    }
+
+    void debit(){
+        System.out.print("Enter withdrawal amount multiple of 100: ");
+		int amt = input.nextInt();
+		if(amt%100==0) calcCheckingWithdraw(amt);
+		else System.out.print("Invalid Amount");
+    }
+
+	public void pinChange() {
+		System.out.print("Enter the old Pin Number: ");
+		int pin = input.nextInt();
+		if(pin==pinNumber){
+			System.out.print("Enter the New Pin Number: ");
+			pin = input.nextInt();
+			pinNumber = pin;
+		}
+		else{
+			System.out.print("!!Invalid Pin Number!!");
+		}
+	}
+}
+class OptionMenu {
+	Scanner menuInput = new Scanner(System.in);
+	DecimalFormat moneyFormat = new DecimalFormat("'Rs.'###,##0.00");
+	HashMap<Integer, Account> data = new HashMap<Integer, Account>();
+
+	public void getLogin() throws IOException {
+		boolean end = false;
+		int customerNumber = 0;
+		int pinNumber = 0;
+		while (!end) {
+			try {
+				System.out.print("\nEnter your customer number: ");
+				customerNumber = menuInput.nextInt();
+				System.out.print("\nEnter your PIN number: ");
+				pinNumber = menuInput.nextInt();
+				Iterator it = data.entrySet().iterator();
+				while (it.hasNext()) {
+					Map.Entry pair = (Map.Entry) it.next();
+					Account acc = (Account) pair.getValue();
+					if (data.containsKey(customerNumber) && pinNumber == acc.getPinNumber()) {
+						execute(acc);
+						end = true;
+						break;
+					}
+				}
+				if (!end) {
+					System.out.println("\nWrong Customer Number or Pin Number");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("\nInvalid Character(s). Only Numbers.");
+				break;
+			}
+		}
+	}
+
+
+	public void execute(Account acc) {
+		boolean end = false;
+		while (!end) {
+			try {
+				System.out.println("\nCheckings Account: ");
+				System.out.println(" Type 1 - Credit");
+				System.out.println(" Type 2 - Debit");
+				System.out.println(" Type 3 - Balance Enquiry");
+				System.out.println(" Type 4 - Pin Change");
+				System.out.println(" Type 5 - Exit");
+				System.out.print("\nChoice: ");
+
+				int selection = menuInput.nextInt();
+
+				switch (selection) {
+				case 3:
+					System.out.println("\nCheckings Account Balance: " + moneyFormat.format(acc.getCheckingBalance()));
+					break;
+				case 1:
+					acc.credit();
+					break;
+				case 2:
+					acc.debit();
+					break;
+
+				case 4:
+					acc.pinChange();
+					break;
+				case 5:
+					end = true;
+					break;
+				default:
+					System.out.println("\nInvalid Choice.");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("\nInvalid Choice.");
+				menuInput.next();
+			}
+		}
+	}
+
+	public void createAccount() throws IOException {
+		int cst_no = 0;
+		boolean end = false;
+		while (!end) {
+			try {
+				System.out.println("\nEnter your customer number ");
+				cst_no = menuInput.nextInt();
+				Iterator it = data.entrySet().iterator();
+				while (it.hasNext()) {
+					Map.Entry pair = (Map.Entry) it.next();
+					if (!data.containsKey(cst_no)) {
+						end = true;
+					}
+				}
+				if (!end) {
+					System.out.println("\nThis customer number is already registered");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("\nInvalid Choice.");
+				menuInput.next();
+			}
+		}
+		System.out.println("\nEnter PIN to be registered");
+		int pin = menuInput.nextInt();
+		data.put(cst_no, new Account(cst_no, pin));
+		System.out.println("\nYour new account has been successfuly registered!");
+		System.out.println("\nRedirecting to login.............");
+		getLogin();
+	}
+
+	public void mainMenu() throws IOException {
+		data.put(952141, new Account(952141, 191904, 1000));
+		data.put(123, new Account(123, 123, 20000));
+		boolean end = false;
+		while (!end) {
+			try {
+				System.out.println("\n Type 1 - Login");
+				System.out.println(" Type 2 - Create Account");
+				System.out.print("\nChoice: ");
+				int choice = menuInput.nextInt();
+				switch (choice) {
+				case 1:
+					getLogin();
+					end = true;
+					break;
+				case 2:
+					createAccount();
+					end = true;
+					break;
+				default:
+					System.out.println("\nInvalid Choice.");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("\nInvalid Choice.");
+				menuInput.next();
+			}
+		}
+		System.out.println("\nThank You for using this ATM.\n");
+		menuInput.close();
+		System.exit(0);
+	}
+}
+
