2201d0cd86f8e1746d546c707157954575e1ebf3
Sonu Daryani <100096513+sonu7524@users.noreply.github.com>
2022-12-09 20:53:51 +0530

    Option menu show interface

---
+++ OptionMenu.java
@@ -0,0 +1,139 @@
+class OptionMenu {
+	Scanner menuInput = new Scanner(System.in);
+	DecimalFormat moneyFormat = new DecimalFormat("'Rs.'###,##0.00");
+	HashMap<Integer, Account> data = new HashMap<Integer, Account>();
+
+	public void getLogin() throws IOException {
+		boolean end = false;
+		int customerNumber = 0;
+		int pinNumber = 0;
+		while (!end) {
+			try {
+				System.out.print("\nEnter your customer number: ");
+				customerNumber = menuInput.nextInt();
+				System.out.print("\nEnter your PIN number: ");
+				pinNumber = menuInput.nextInt();
+				Iterator it = data.entrySet().iterator();
+				while (it.hasNext()) {
+					Map.Entry pair = (Map.Entry) it.next();
+					Account acc = (Account) pair.getValue();
+					if (data.containsKey(customerNumber) && pinNumber == acc.getPinNumber()) {
+						execute(acc);
+						end = true;
+						break;
+					}
+				}
+				if (!end) {
+					System.out.println("\nWrong Customer Number or Pin Number");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("\nInvalid Character(s). Only Numbers.");
+				break;
+			}
+		}
+	}
+
+
+	public void execute(Account acc) {
+		boolean end = false;
+		while (!end) {
+			try {
+				System.out.println("\nCheckings Account: ");
+				System.out.println(" Type 1 - Credit");
+				System.out.println(" Type 2 - Debit");
+				System.out.println(" Type 3 - Balance Enquiry");
+				System.out.println(" Type 4 - Pin Change");
+				System.out.println(" Type 5 - Exit");
+				System.out.print("\nChoice: ");
+
+				int selection = menuInput.nextInt();
+
+				switch (selection) {
+				case 3:
+					System.out.println("\nCheckings Account Balance: " + moneyFormat.format(acc.getCheckingBalance()));
+					break;
+				case 1:
+					acc.credit();
+					break;
+				case 2:
+					acc.debit();
+					break;
+
+				case 4:
+					acc.pinChange();
+					break;
+				case 5:
+					end = true;
+					break;
+				default:
+					System.out.println("\nInvalid Choice.");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("\nInvalid Choice.");
+				menuInput.next();
+			}
+		}
+	}
+
+	public void createAccount() throws IOException {
+		int cst_no = 0;
+		boolean end = false;
+		while (!end) {
+			try {
+				System.out.println("\nEnter your customer number ");
+				cst_no = menuInput.nextInt();
+				Iterator it = data.entrySet().iterator();
+				while (it.hasNext()) {
+					Map.Entry pair = (Map.Entry) it.next();
+					if (!data.containsKey(cst_no)) {
+						end = true;
+					}
+				}
+				if (!end) {
+					System.out.println("\nThis customer number is already registered");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("\nInvalid Choice.");
+				menuInput.next();
+			}
+		}
+		System.out.println("\nEnter PIN to be registered");
+		int pin = menuInput.nextInt();
+		data.put(cst_no, new Account(cst_no, pin));
+		System.out.println("\nYour new account has been successfuly registered!");
+		System.out.println("\nRedirecting to login.............");
+		getLogin();
+	}
+
+	public void mainMenu() throws IOException {
+		data.put(952141, new Account(952141, 191904, 1000));
+		data.put(123, new Account(123, 123, 20000));
+		boolean end = false;
+		while (!end) {
+			try {
+				System.out.println("\n Type 1 - Login");
+				System.out.println(" Type 2 - Create Account");
+				System.out.print("\nChoice: ");
+				int choice = menuInput.nextInt();
+				switch (choice) {
+				case 1:
+					getLogin();
+					end = true;
+					break;
+				case 2:
+					createAccount();
+					end = true;
+					break;
+				default:
+					System.out.println("\nInvalid Choice.");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("\nInvalid Choice.");
+				menuInput.next();
+			}
+		}
+		System.out.println("\nThank You for using this ATM.\n");
+		menuInput.close();
+		System.exit(0);
+	}
+}
